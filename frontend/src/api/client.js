8013d6a05bac1f208e64e3d10cd3526ba94af5cd
AldoQuispeDev <aldoquispelocumber029@gmail.com>
2025-10-30 20:11:08 -0500

    update: variables de entorno y conexion a Raiway

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: "https://sistema-apv-backend.onrender.com/api",
   withCredentials: true, // ✅ envía y recibe cookies automáticamente
 });
 
 export default api;
+
+
